1aa7a34588abf32bedd951799e3588f76214feea
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-09-12 16:09:20 +0100

    initial project set up

---
+++ components/PlayerListing.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1 class="heading">Players</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PlayerListing'
+}
+</script>
+
+<style scoped>
+
+</style>
