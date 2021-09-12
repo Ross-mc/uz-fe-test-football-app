3b9e1b2bcbc0d2fb302767c1d22badf1f0f7cfab
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-09-12 17:19:17 +0100

    player data is rendered to the page

---
+++ components/PlayerCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <a :class="['player-card', player.position]" :href="profilePageUrl" target="_blank" rel="norefferer">
+    <h3>{{`${player.firstName} ${player.surname}`}}</h3>
+    <div class="info">
+      <div class="country">{{player.country}}</div>
+      <div class="number">{{player.shirtNumber === "unknown" ? "TBC" : player.shirtNumber}}</div>
+      <div class="position">{{player.position}}</div>
+    </div>
+    <div class="img">
+      <img :src="player.squadImage" :alt="`Profile picture of ${player.firstName} ${player.surname}`">
+    </div>
+    <div class="join-date">
+      {{`Signed : ${player.joinDate}`}}
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'PlayerCard',
+  props: {
+    player: {
+      country: String,
+      firstName: String,
+      surname: String,
+      squadImage: String,
+      joinDate: String,
+      position: String,
+      shirtNumber: String,
+      profilePageUrl: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .player-card {
+
+  }
+</style>
--- components/PlayerListing.vue
+++ components/PlayerListing.vue
@@ -1,12 +1,25 @@
 <template>
   <section class="player-listing">
     <h1 class="heading">Players</h1>
+    <section class="player-container">
+      <player-card v-for="player in players" :player="player" :key="player.squadImage"/>
+    </section>
   </section>
 </template>
 
 <script>
+import playerData from '../data/players';
+import PlayerCard from './PlayerCard.vue'
 export default {
-  name: 'PlayerListing'
+  name: 'PlayerListing',
+  components: {
+    PlayerCard
+  },
+  data: function (){
+    return {
+      players: playerData.players
+    }
+  }
 }
 </script>
 
@@ -14,11 +27,25 @@ export default {
   .player-listing{
     min-height: 100vh;
     width: 90%;
-    max-width: 1000px;
+    max-width: 1500px;
     background-color: white;
     margin: 0 auto;
+    padding: 1rem 0.5rem;
   }
   .heading{
     margin: 0;
+  }
+
+  .player-container {
+    display: grid;
+    grid-template-columns: 30% 30% 30%;
+    gap: 5%;
+  }
+
+  @media (max-width: 800px){
+    .player-container{
+      display: flex;
+      flex-direction: column;
+    }
   }
 </style>
